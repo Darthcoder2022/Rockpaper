0fc605117d3551d3b3d7dd316b3b8372210f6200
Darthcoder <htepe87@gmail.com>
2022-08-31 22:43:43 +0300

    Fpnksiyon tamamlandı

--- rock.js
+++ rock.js
@@ -1,7 +1,10 @@
-console.log("Hello World");
+//console.log("Hello World");
 
 const gameList = ["rock", "paper", "scissors"];
 
+let computerScore = 0;
+let tieCounter = 0;
+let playerScore = 0;
 
 
 function getComputerChoice() {
@@ -9,32 +12,65 @@ function getComputerChoice() {
 
 }
 //console.log(getComputerChoice());
-const computerSelection = getComputerChoice().toLowerCase();
-const playerSelection = prompt("Rock,Paper or Scissors").toLowerCase();
+let computerSelection = getComputerChoice().toLowerCase();
+let playerSelection = prompt("Rock,Paper or Scissors").toLowerCase();
+
 
 function playround(playerSelection, computerSelection) {
+    
+    
     if (playerSelection === gameList[0] && computerSelection === gameList[2]) {
+        playerScore++;
         return "You win Rock beats Scissors";
+        
 
     } else if (playerSelection === gameList[1] && computerSelection === gameList[0]) {
+        playerScore++;
         return "You win Paper beats Rock";
 
     } else if (playerSelection === gameList[2] && computerSelection === gameList[1]) {
+        playerScore++;
         return "You win Scissors beats Paper";
     } else if (playerSelection === gameList[2] && computerSelection === gameList[0]) {
+        computerScore++;
         return "You lose Rock beats Scissors";
     } else if (playerSelection === gameList[1] && computerSelection === gameList[2]) {
+        computerScore++;
         return "You lose Scissors beats Paper";
 
+
     } else if (playerSelection === gameList[0] && computerSelection === gameList[1]) {
+        computerScore++;
         return "You lose Paper beats Rock";
 
     }else if(playerSelection===computerSelection){
-        return "It is a tie"
+        tieCounter++
+        return "It is a tie";
     }
 
 }
 
 console.log(playround(playerSelection,computerSelection));
 
+function game(){
+    for(let i = 0; i<4 ; i++){
+        computerSelection=getComputerChoice().toLowerCase();
+        playerSelection = prompt("Rock,Paper or Scissors").toLowerCase();
+        console.log(playround(playerSelection,computerSelection));
+        console.log("Your score : "+String(playerScore)+" Computer score: "+String(computerScore)+" and there is: "+String(tieCounter)+" tie.");
+       
+    }
+    console.log("Your score : "+String(playerScore)+" Computer score: "+String(computerScore)+" and there is: "+String(tieCounter)+" tie.");
+}
+
+game();
+/*for(let i=0;i<5;i++){
+   computerSelection=getComputerChoice().toLowerCase();
+   playerSelection = prompt("Rock,Paper or Scissors").toLowerCase();
+   console.log(playround(playerSelection,computerSelection));
+   console.log("Your score : "+String(playerScore)+" Computer score: "+String(computerScore)+" and there is: "+String(tieCounter));
+
+    
+}
+*/
 
